9542c499138be6cc7d84a4b92918f621f52271e1
zablotskyi0121 <sanjzabla.1210@gmail.com>
2020-11-11 21:27:59 +0200

    showed products from DB on the front

--- pub/index.php
+++ pub/index.php
@@ -1,4 +1,30 @@
-<?php
-test test
-echo 'Test';
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php
+        include "db.php";
+        
+        $result = $db_connection->query('SELECT * FROM Ism.products');
+
+        while( $phones = $result->fetch(PDO::FETCH_ASSOC) ){
+            
+            echo ' ID: ' . $phones['Id'] . ', SKU: ' . $phones['SKU'] . ', Phone: ' . $phones['Name'] 
+                    . ', price: ' . $phones['Price'] . ' , description: ' . $phones['Description'] . '<br>';
+        }
+        ?>
+        
+        <div class="image">
+            <img src="media/images/iphone5.jpg" width="400px", alt=""> 
+            <img src="media/images/iphone6.jpg" width="400px", alt="">
+            <img src="media/images/iphone7.jpg" width="400px", alt="">
+            <img src="media/images/iphone8.jpg" width="400px", alt="">
+            <img src="media/images/iphoneX.jpg" width="400px", alt="">
+         </div>   
+
+    </body>
+</html>
 
