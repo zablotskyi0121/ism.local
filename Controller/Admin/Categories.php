5bd595dc5212fc87ef87f0cbe81734db5f4a7e4a
zablotskyi0121 <sanjzabla.1210@gmail.com>
2021-02-20 23:39:13 +0200

    Added CRUD for categories

--- Controller/Admin/Categories.php
+++ Controller/Admin/Categories.php
@@ -11,4 +11,58 @@ class Categories {
         $renderer = $renderer->render('Admin/Category/CategoryList', ['categoryList' => $categoryList]);
     }
 
+    public function actionCreate() {
+
+        $renderer = new \System\Renderer();
+        $renderer = $renderer->render('Admin/Category/CreateCategory');
+
+        if (isset($_POST['submit'])) {
+            $name = $_POST['name'];
+            $description = $_POST['description'];
+
+            $id = \Model\Admin\Categories::createCategory($name, $description);
+
+            if ($id) {
+                if (is_uploaded_file($_FILES["image"]["tmp_name"])) {
+                    move_uploaded_file($_FILES["image"]["tmp_name"], _DIR_PUB_ . "/media/images/{$id}.jpg");
+                }
+            };
+
+            header("Location: /admin/categories/index");
+        }
+    }
+
+    public function actionUpdate($id) {
+
+        $category = \Model\Admin\Categories::getCategoryById($id);
+        $renderer = new \System\Renderer();
+        $renderer = $renderer->render('Admin/Category/UpdateCategory', ['category' => $category]);
+
+        if (isset($_POST['submit'])) {
+            $name = $_POST['name'];
+            $description = $_POST['description'];
+
+            if (\Model\Admin\Categories::updateCategoryById($id, $name, $description)) {
+                if (is_uploaded_file($_FILES["image"]["tmp_name"])) {
+
+                    move_uploaded_file($_FILES["image"]["tmp_name"], _DIR_PUB_ . "/media/images/{$id}.jpg");
+                }
+            }
+
+            header("Location: /admin/categories/index");
+        }
+    }
+
+    public function actionDelete($id) {
+
+        $renderer = new \System\Renderer();
+        $renderer = $renderer->render('Admin/Category/DeleteCategory', ['id' => $id]);
+
+        if (isset($_POST['submit'])) {
+            \Model\Admin\Categories::deleteCategoryById($id);
+            header("Location: /admin/categories/index");
+        }
+        return true;
+    }
+
 }
--- Model/Admin/Categories.php
+++ Model/Admin/Categories.php
@@ -3,12 +3,12 @@
 namespace Model\Admin;
 
 class Categories {
-    
-    public static function getCategoriesList(){
-        
+
+    public static function getCategoriesList() {
+
         $db = \System\Db::getInstance()->getInstance();
         $result = $db->query('SELECT id, name, description, image FROM categories ORDER BY id ASC');
-        
+
         $categoryList = array();
         $i = 0;
         while ($category = $result->fetch()) {
@@ -20,4 +20,46 @@ class Categories {
         }
         return $categoryList;
     }
+
+    public static function createCategory($name, $description) {
+
+        $db = \System\Db::getInstance()->getInstance();
+        $result = $db->prepare('INSERT INTO categories (id, name, description) VALUES (NULL, :name, :description)');
+        $result->bindParam(':name', $name, \PDO::PARAM_STR);
+        $result->bindParam(':description', $description, \PDO::PARAM_STR);
+        if ($result->execute()) {
+            return $db->lastInsertId();
+        }
+        echo 'error';
+    }
+
+    public static function updateCategoryById($id, $name, $description) {
+
+        $db = \System\Db::getInstance();
+        $result = $db->prepare('UPDATE categories SET name = :name, description = :description WHERE id = :id');
+        $result->bindParam(':id', $id, \PDO::PARAM_INT);
+        $result->bindParam(':name', $name, \PDO::PARAM_STR);
+        $result->bindParam(':description', $description, \PDO::PARAM_STR);
+        $result->execute();
+        return $result;
+    }
+
+    public static function getCategoryById($id) {
+
+        $db = \System\Db::getInstance()->getInstance();
+        $result = $db->prepare('SELECT * FROM categories WHERE id = :id');
+        $result->bindParam(':id', $id, \PDO::PARAM_INT);
+        $result->setFetchMode(\PDO::FETCH_ASSOC);
+        $result->execute();
+        return $result->fetch();
+    }
+
+    public static function deleteCategoryById($id) {
+
+        $db = \System\Db::getInstance()->getInstance();
+        $result = $db->prepare('DELETE FROM categories WHERE id = :id');
+        $result->bindParam(':id', $id, \PDO::PARAM_INT);
+        return $result->execute();
+    }
+
 }
