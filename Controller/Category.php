bb50d9c2f175797694cc29d55c14c3683b8db5af
zablotskyi0121 <sanjzabla.1210@gmail.com>
2021-04-06 20:56:21 +0300

    removed actionList

--- Controller/Category.php
+++ Controller/Category.php
@@ -4,12 +4,6 @@ namespace Controller;
 
 class Category {
 
-    public function actionList() {
-
-        $categories = \Model\Category::getCategoriesList();
-        \System\Renderer::render('Menu', ['categories' => $categories]);
-    }
-
     public function actionView($categoryId) {
 
         $productList = \Model\Product::getProductsPerCategoty($categoryId);
