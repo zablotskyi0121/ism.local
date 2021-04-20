ebee6d9156a7f3d14b39b306ef3409cccc9d47fd
zablotskyi0121 <sanjzabla.1210@gmail.com>
2021-04-20 20:24:36 +0300

    fixed order grid

--- Model/Admin/Order.php
+++ Model/Admin/Order.php
@@ -7,12 +7,13 @@ class Order {
     public static function getOrdersList() {
 
         $db = \System\Db::getInstance()->getPDO();
-        $result = $db->query('SELECT `id`, `userId`, `created`, `userName`, `userEmail`, `userPhone`, `userComment` FROM `order` ORDER BY id ASC ');
+        $result = $db->query('SELECT `id`, `totalPrice`, `userId`, `created`, `userName`, `userEmail`, `userPhone`, `userComment` FROM `order` ORDER BY id ASC ');
 
         $orderList = array();
         $i = 0;
         while ($order = $result->fetch()) {
             $orderList[$i]['id'] = $order['id'];
+            $orderList[$i]['totalPrice'] = $order['totalPrice'];
             $orderList[$i]['userId'] = $order['userId'];
             $orderList[$i]['created'] = $order['created'];
             $orderList[$i]['userName'] = $order['userName'];
